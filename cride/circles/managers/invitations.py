592a6ba2cf57f165cbd73ac886328cf0ecac3c8c
Hugo <gthugo@outlook.com>
2022-12-26 20:45:02 -0500

    Code to make an invitation

---
+++ cride/circles/managers/invitations.py
@@ -0,0 +1,25 @@
+"""Circle invitation managers"""
+
+from random import choices
+from string import ascii_uppercase, digits
+
+from django.db import models
+
+
+class InvitationManager(models.Manager):
+    """Invitation manager
+
+    Used to handle code creation
+    """
+
+    CODE_LENGTH = 10
+
+    def create(self, **kwargs):
+        """Handle code creation"""
+        pool = ascii_uppercase + digits + '.-'
+        code = kwargs.get('code', choices(pool, k=self.CODE_LENGTH))
+        while self.filter(code=code).exists():
+            code = ''.join(choices(pool, k=self.CODE_LENGTH))
+        kwargs['code'] = code
+
+        return super(InvitationManager, self).create(**kwargs)
--- cride/circles/models/__init__.py
+++ cride/circles/models/__init__.py
@@ -1,2 +1,3 @@
 from .circles import Circle
+from .invitations import InvitationManager
 from .memberships import Membership
